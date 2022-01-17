7b4e445da9fc6ab6f4032cd1d09df3deef0efae3
Juan Ceballos <jacu29@gmail.com>
2022-01-17 01:40:35 -0500

    Agregar desplegable Cursos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,17 @@ import { IoSearch, IoCaretDown, IoCaretUp, IoMenu } from "react-icons/io5";
 
 import logo from "../images/logo.png"
 
+import bulletPoint1 from "../images/courses-list/002-gantt chart.png";
+import bulletPoint2 from "../images/courses-list/021-mail.png";
+import bulletPoint3 from "../images/courses-list/029-video marketing.png";
+import bulletPoint4 from "../images/courses-list/040-Branching.png";
+import bulletPoint5 from "../images/courses-list/063-heart.png";
+import bulletPoint6 from "../images/courses-list/078-internet.png";
+import bulletPoint7 from "../images/courses-list/093-idea.png";
+
+import coursesPreviewList from "../utils/previewCourses";
+import SlideShow from "./sliders/SlideShow";
+
 const Navbar = () => {
         const [menu, setMenu] = useState(false);
         const [showMenu, setShowMenu] = useState("none");
@@ -46,7 +57,7 @@ const Navbar = () => {
         <div className="Navbar">
             <div className="navbar-container container">
                 <div className="home">
-                    <a className='logo' href="/#">
+                    <a className="logo" href="/#">
                         <img src={logo} alt="Libel Academy" />
                     </a>
                 </div>
@@ -107,6 +118,131 @@ const Navbar = () => {
                                 setMenu(!menu);
                             }}
                         />
+                    </div>
+                </div>
+                <ul className="menu-list" style={{ display: `${showMenu}` }}>
+                    <div className="menu-academy">
+                        <li>Crear cuenta</li>
+                        <li>Entrar</li>
+                    </div>
+                    <div className="menu-shortcuts">
+                        <li>Licencias</li>
+                        <li>Cursos</li>
+                    </div>
+                </ul>
+            </div>
+            <div
+                className="courses-menu-container"
+                style={coursesMenu}
+                onClick={handleCloseCourseMenu}
+            >
+                <div className="courses-menu" style={{ top: `${top}` }}>
+                    <div className="courses-list">
+                        <div className="courses-column">
+                            <h3>TODOS LOS CURSOS</h3>
+                            <ul>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint7}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Curso de Rigging</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint6}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Curso de Blender</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint5}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Curso de ZBrush</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint4}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Curso de ilustración</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="courses-column">
+                            <h3>ENTRENAMIENTOS</h3>
+                            <ul>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint1}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Masters</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint2}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Membresías</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint3}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">3D Camp</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint4}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Aprende a tu ritmo</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint5}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Licencias ZBrush</a>
+                                </li>
+                                <li>
+                                    <div className="corses-bullet-point">
+                                        <img
+                                            src={bulletPoint6}
+                                            alt="bullet point"
+                                        />
+                                    </div>
+                                    <a href="/#">Reallusion</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="courses-preview">
+                        {courses ? (
+                            <SlideShow courses={coursesPreviewList} />
+                        ) : null}
                     </div>
                 </div>
             </div>
